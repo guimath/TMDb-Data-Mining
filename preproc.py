d677da5b1a0e9113728e54f904088335a6bcec44
Guilhem Mathieux <guilhem.mathieux@telecom-paris.fr>
2024-03-26 15:23:48 +0800

    changing dataset

--- preproc.py
+++ preproc.py
@@ -3,12 +3,10 @@ import os
 
 
 def main():
-    if not os.path.exists('data/TMDB_movie_dataset_v11.csv'):
+    if not os.path.exists('data/TMBD Movie Dataset.csv'):
         import kaggle
-        print('no csv found, downloading...', end='\r')
         kaggle.api.authenticate()
-        kaggle.api.dataset_download_files('asaniczka/tmdb-movies-dataset-2023-930k-movies', path='data', unzip=True)
-        print('Done downloading TMDB_movie_dataset_v11.csv')
+        kaggle.api.dataset_download_files('successikuku/tmbd-movie-dataset', path='data', quiet=False, unzip=True)
 
 if __name__ == '__main__':
     main()
